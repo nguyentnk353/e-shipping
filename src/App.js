9d4880a04c78ebfcb5ef6132cbaa7adc4ecd2acf
keru01 <tontrongnghia2k1@gmail.com>
2022-10-29 23:36:05 +0700

    add .eslintrc and add funtion in page check-charges

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.less';
+import GuestFooter from './components/GuestFooter/GuestFooter';
 import { GuestNavbar } from './components/GuestNavbar/GuestNavbar';
-import User from './pages/user/User';
+import GuestCheckCharges from './pages/guest/GuestCheckCharges/GuestCheckCharges';
 import GuestHome from './pages/guest/GuestHome/GuestHome';
 import GuestService from './pages/guest/GuestService/GuestService';
-import GuestFooter from './components/GuestFooter/GuestFooter';
-import GuestCheckCharges from './pages/guest/GuestCheckCharges/GuestCheckCharges';
 
 function App() {
   const role = 'user';
@@ -16,7 +15,7 @@ function App() {
         <Route path='/' element={<GuestHome />} />
         <Route path='/home' element={<GuestHome />} />
         <Route path='/service' element={<GuestService />} />
-        <Route path='/check-charge' element={<GuestCheckCharges />} />
+        <Route path='/check-charge' element={<GuestCheckCharges />} />       
       </Routes>
       <GuestFooter />
     </div>
