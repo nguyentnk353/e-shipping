7f6b9514f6ee794a69779e10a2cc365866045190
keru01 <tontrongnghia2k1@gmail.com>
2022-10-29 23:39:57 +0700

    fix commit in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.less';
-import GuestFooter from './components/GuestFooter/GuestFooter';
-import { GuestNavbar } from './components/GuestNavbar/GuestNavbar';
-import GuestCheckCharges from './pages/guest/GuestCheckCharges/GuestCheckCharges';
 import GuestHome from './pages/guest/GuestHome/GuestHome';
 import GuestService from './pages/guest/GuestService/GuestService';
+import GuestCheckCharges from './pages/guest/GuestCheckCharges/GuestCheckCharges';
+import UserHome from './pages/user/UserHome/UserHome';
+import UserService from './pages/user/UserService/UserService';
+import Guest from './pages/guest/Guest/Guest';
+import User from './pages/user/User/User';
 
 function App() {
   const role = 'user';
   return (
     <div className='App'>
-      <GuestNavbar />
       <Routes>
-        <Route path='/' element={<GuestHome />} />
-        <Route path='/home' element={<GuestHome />} />
-        <Route path='/service' element={<GuestService />} />
-        <Route path='/check-charge' element={<GuestCheckCharges />} />       
+        <Route path='/guest' element={<Guest />}>
+          <Route path='home' element={<GuestHome />} />
+          <Route path='service' element={<GuestService />} />
+          <Route path='check-charge' element={<GuestCheckCharges />} />
+        </Route>
+        <Route path='/user' element={<User />}>
+          <Route path='home' element={<UserHome />} />
+          <Route path='service' element={<UserService />} />
+        </Route>
       </Routes>
-      <GuestFooter />
     </div>
   );
 }
