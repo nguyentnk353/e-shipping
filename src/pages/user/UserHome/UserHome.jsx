923661c7968a9c535b86d99533dba20ac0ec9041
Huy <huydnqse151303@fpt.edu.vn>
2022-10-31 01:04:58 +0700

    user navbar and sidebar

--- src/pages/user/UserHome/UserHome.jsx
+++ src/pages/user/UserHome/UserHome.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
+import { UserNavbar } from "../../../components/UserNavbar/UserNavbar";
+import UserSidebar from "../../../components/UserSidebar/UserSidebar";
 
 const UserHome = () => {
-  return <div>UserHome</div>;
+  return (
+    <div>
+      <UserNavbar/>
+      <UserSidebar />
+    </div>
+  );
 };
 
 export default UserHome;
