81c9bef0d8344c41de4a0cdbe3030b93413ba3fb
keru01 <tontrongnghia2k1@gmail.com>
2022-11-01 13:07:34 +0700

    Fix Fe page create-new-order

--- src/pages/user/UserAddOrder/UserAddOrder.jsx
+++ src/pages/user/UserAddOrder/UserAddOrder.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './UserAddOrder.css'
-import { UserOutlined, ProfileFilled, CaretDownFilled, CodeSandboxSquareFilled } from '@ant-design/icons';
-import { Button, Select, Form } from 'antd';
+import { UserOutlined, CaretDownFilled, CodeSandboxSquareFilled } from '@ant-design/icons';
+import { Select, Form } from 'antd';
 import 'antd/dist/antd.css';
-// import UserTest from '../../../components/UserNavbar/UserTest/UserTest';
-// import UserTest2 from '../../../components/UserNavbar/UserTest/UserTest2';
+import UserTest from '../../../components/UserNavbar/UserTest/UserTest';
+import UserTest2 from '../../../components/UserNavbar/UserTest/UserTest2';
 
 
 
@@ -13,147 +13,10 @@ function UserAddOrder() {
     return (
 
         <div>
-            {/* <UserTest /> */}
+            <UserTest />
             <div style={{ display: 'flex' }}>
-                {/* <UserTest2 /> */}
+                <UserTest2 />
                 <div className='page-add-order'>
-                <div className='frame-merchandise-service'>
-                        <div>
-                            <div className='frame service'>
-                                <div className='content service'>
-                                    <div className='title-frames'>
-                                        <CodeSandboxSquareFilled className='icon-page-service' />
-                                        <div style={{ paddingRight: '50%' }}>Thông tin dịch vụ - Hàng hóa</div>
-                                    </div>
-                                    <hr color='black' />
-
-                                    <div style={{ fontWeight: '700' }}>
-                                        <Form.Item
-                                            label="Địa chỉ gửi hàng"
-                                            name='Address-send'
-                                            rules={[{ required: true }]}
-                                        >
-                                            <div style={{ fontWeight: '480' }}>
-                                                <Select
-                                                    showSearch
-                                                    placeholder="Chọn tỉnh thành"
-                                                    suffixIcon={dropIcon}
-                                                    size='large'
-                                                    style={{
-                                                        width: '565px',
-                                                        textAlign: 'left',
-                                                        fontSize: '14px',
-                                                    }}
-                                                    optionFilterProp="children"
-                                                    filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
-                                                // options={data.Provices}
-                                                >
-                                                </Select>
-                                            </div>
-                                        </Form.Item>
-                                    </div>
-
-
-                                    <div>
-                                        <div className='input-every-2-8'>
-                                            <Form.Item
-                                                label="Địa chỉ nhận hàng"
-                                                name='Address-receiver'
-                                                rules={[{ required: true }]}
-                                            >
-                                                <input type='text' className='input-every-2' placeholder='Địa chỉ nhận hàng' />
-                                            </Form.Item>
-                                        </div>
-                                    </div>
-
-                                    <div className='input-3-5'>
-                                        <Select
-                                            showSearch
-                                            placeholder="Tỉnh/Thành phố"
-                                            suffixIcon={dropIcon}
-                                            size='large'
-                                            style={{
-                                                width: '32%',
-                                                textAlign: 'left',
-                                                fontSize: '14px',
-                                            }}
-                                            optionFilterProp="children"
-                                            filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
-                                        // options={data.Provices}
-                                        >
-                                        </Select>
-                                        <Select
-                                            showSearch
-                                            placeholder="Địa chỉ gửi hàng"
-                                            suffixIcon={dropIcon}
-                                            size='large'
-                                            style={{
-                                                width: '32%',
-                                                textAlign: 'left',
-                                                fontSize: '14px',
-                                            }}
-                                            optionFilterProp="children"
-                                            filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
-                                        // options={data.Provices}
-                                        >
-                                        </Select>
-                                        <Select
-                                            showSearch
-                                            placeholder="Địa chỉ gửi hàng"
-                                            suffixIcon={dropIcon}
-                                            size='large'
-                                            style={{
-                                                width: '32%',
-                                                textAlign: 'left',
-                                                fontSize: '14px',
-                                            }}
-                                            optionFilterProp="children"
-                                            filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
-                                        // options={data.Provices}
-                                        >
-                                        </Select>
-                                    </div><br />
-
-                                    <div style={{ display: 'flex' }}>
-                                        <div className='input-every-2-8'>
-                                            <Form.Item
-                                                label="Loại hàng hóa"
-                                                name='Type-merchandise'
-                                                rules={[{ required: true }]}
-                                            >
-                                                <div>
-                                                    <Select
-                                                        showSearch
-                                                        suffixIcon={dropIcon}
-                                                        size='large'
-                                                        style={{
-                                                            width: '265px',
-                                                            textAlign: 'left',
-                                                            fontSize: '14px',
-                                                        }}
-                                                        optionFilterProp="children"
-                                                        filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
-                                                    // options={data.Provices}
-                                                    >
-                                                    </Select>
-                                                </div>
-                                            </Form.Item>
-                                        </div>
-                                        <div className='input-every-2-8'>
-                                            <Form.Item
-                                                label="Khối lượng"
-                                                name='Weight'
-                                                rules={[{ required: true }]}
-                                            >
-                                                <div><input type='text' className='input-every 1' placeholder='Khối lượng' /></div>
-                                            </Form.Item>
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
                     <div className='frame-sender-receiver'>
                         <div>
                             <div className='frame sender'>
@@ -192,10 +55,7 @@ function UserAddOrder() {
                             <div className='content receiver'>
                                 <div className='title-frames'>
                                     <UserOutlined className='icon-page-add' />
-                                    <div style={{ paddingRight: '38%' }}>Nguời nhận</div>
-                                    <Button type="primary" icon={<ProfileFilled className='icon-page-add' />}>
-                                        Danh sách địa chỉ nhận
-                                    </Button>
+                                    <div style={{ paddingRight: '75%' }}>Nguời nhận</div>
                                 </div>
                                 <hr color='black' />
 
@@ -222,10 +82,130 @@ function UserAddOrder() {
                                     </div>
                                 </div>
                             </div>
-                            <button className='btn-page-create-new-order'>TẠO ĐƠN HÀNG</button>
-                        </div>                          
-                        
+
+                        </div>
                     </div>
+
+                    <div className='frame-merchandise-service'>
+                        <div>
+                            <div className='frame service'>
+                                <div className='content service'>
+                                    <div className='title-frames'>
+                                        <CodeSandboxSquareFilled className='icon-page-service' />
+                                        <div style={{ paddingRight: '50%' }}>Thông tin dịch vụ - Hàng hóa</div>
+                                    </div>
+                                    <hr color='black' />
+
+                                    <div style={{ fontWeight: '700' }}>
+                                        <Form.Item
+                                            label="Bưu cục gửi hàng"
+                                            name='Address-send'
+                                            rules={[{ required: true }]}
+                                            style={{ height: '13px', marginBottom:'27px' }}
+                                        >
+                                        </Form.Item>
+                                        <div style={{ fontWeight: '480', margin:'10px 0' }}>
+                                            <Select
+                                                showSearch
+                                                placeholder="Tỉnh/Thành phố"
+                                                suffixIcon={dropIcon}
+                                                size='large'
+                                                style={{
+                                                    width: '265px',
+                                                    textAlign: 'left',
+                                                    fontSize: '14px',
+                                                    marginRight: '100%'
+                                                }}
+                                                optionFilterProp="children"
+                                                filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
+                                            // options={data.Provices}
+                                            >
+                                            </Select>
+                                        </div>
+
+                                    </div>
+
+
+                                    <div>
+                                        <div className='input-every-2-8'>
+                                            <Form.Item
+                                                label="Bưu cục nhận hàng"
+                                                name='Address-receiver'
+                                                rules={[{ required: true }]}
+                                                style={{ height: '13px' }}
+
+                                            >
+                                            </Form.Item>
+                                            <div className='input-3-5'>
+                                                <div className='input-new-5-7-9'>
+                                                <Select
+                                                    showSearch
+                                                    placeholder="Tỉnh/Thành phố"
+                                                    suffixIcon={dropIcon}
+                                                    size='large'
+                                                    style={{
+                                                        width: '265px',
+                                                        textAlign: 'left',
+                                                        fontSize: '14px',
+                                                        fontWeight: '400',
+                                                    }}
+                                                    optionFilterProp="children"
+                                                    filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
+                                                // options={data.Provices}
+                                                >
+                                                </Select>
+                                                </div>
+
+                                                <input type='text' className='input-every-2' placeholder='Địa chỉ chi tiết' />
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div style={{ display: 'flex' }}>
+                                        <div className='input-every-2-8'>
+                                            <Form.Item
+                                                label="Loại hàng hóa"
+                                                name='Type-merchandise'
+                                                rules={[{ required: true }]}
+                                            >
+                                                <div >
+                                                    <Select
+                                                        showSearch
+                                                        className='input-new-5-7-9'
+                                                        suffixIcon={dropIcon}
+                                                        placeholder="Chọn loại hàng hóa"
+                                                        size='large'
+                                                        style={{
+                                                            width: '265px',
+                                                            textAlign: 'start',
+                                                            fontSize: '14px',
+                                                            fontWeight: '400',
+                                                        }}
+                                                        optionFilterProp="children"
+                                                        filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
+                                                    // options={data.Provices}
+                                                    >
+                                                    </Select>
+                                                </div>
+                                            </Form.Item>
+                                        </div>
+                                        <div className='input-every-2-8' style={{marginLeft:'17px'}}>
+                                            <Form.Item
+                                                label="Khối lượng"
+                                                name='Weight'
+                                                rules={[{ required: true }]}
+                                            >
+                                                <div><input type='text' className='input-every 1' placeholder='Khối lượng' /></div>
+                                            </Form.Item>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <button className='btn-page-create-new-order'>TẠO ĐƠN HÀNG</button>
+                            </div>
+                        </div>
+                    </div>
+
 
                 </div>
             </div>
