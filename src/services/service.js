fae69cc4043ce8590a91fbc4bf5668a44f9bf80e
keru01 <tontrongnghia2k1@gmail.com>
2022-11-07 14:55:05 +0700

    validate: page create-new-order

---
+++ src/services/service.js
@@ -0,0 +1,17 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     // Mong muốn của chúng ta
+//     Validator({
+//       form: '#form-create-new-order',
+//       errorSelector: '.form-message',
+//       rules: [
+//         Validator.isRequired('#fullname', 'Vui lòng nhập tên đầy đủ của bạn'),
+//         Validator.minLength('#password', 6),
+//         Validator.isRequired('#password_confirmation')
+//       ],
+//       onSubmit: function (data) {
+//         // Call API
+//         console.log(data);
+//       }
+//     });
+  
+//   });
