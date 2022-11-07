3b812d824bbfd90027efc2bfd29891019ff1cc22
keru01 <tontrongnghia2k1@gmail.com>
2022-11-07 19:16:28 +0700

    Fix: validator

--- src/services/validator.js
+++ src/services/validator.js
@@ -174,3 +174,20 @@ Validator.isConfirmed = function (selector, getConfirmValue, message) {
         }
     }
 }
+// document.addEventListener('DOMContentLoaded', function () {
+//     // Mong muốn của chúng ta
+//     Validator({
+//       form: '#form-create-new-order',
+//       errorSelector: '.form-message',
+//       rules: [
+//         Validator.isRequired('#fullname', 'Vui lòng nhập tên đầy đủ của bạn'),
+//         Validator.minLength('#password', 7),
+//         Validator.isRequired('#password_confirmation')
+//       ],
+//       onSubmit: function (data) {
+//         // Call API
+//         console.log(data);
+//       }
+//     });
+  
+//   });
