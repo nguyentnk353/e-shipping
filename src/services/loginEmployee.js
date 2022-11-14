f01ba317842d083ceddcdb01ad506a8ae597bda0
Khoinguyen107 <knguyen9047@gmail.com>
2022-11-14 19:12:38 +0700

    push code before pull 14-11

--- src/services/loginEmployee.js
+++ src/services/loginEmployee.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-
+import axiosInstance from './axiosInstance';
 export function loginEmployee(props) {
   const url = 'https://deliver-store.tk/api/v1/employee/authenticate';
   const LoginName = props.username;
   const Password = props.password;
   const postData = {};
   const error = { error: 'login-fail' };
-  return axios
+  return axiosInstance
     .post(url, postData, {
       params: {
         loginName: LoginName,
